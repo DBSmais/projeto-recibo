3e4c91d6bcf05f459d0fb099356cf5f4a22d8d1a
Seu Nome <derek.santos0818@gmail.com>
2025-03-09 02:44:50 -0300

    primeiro commit: aperfeiçoa em tudo.

---
+++ settings.gradle.kts
@@ -0,0 +1,25 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+                google()
+                mavenCentral()
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "gerador de recibo 1.0"
+include(":app")
